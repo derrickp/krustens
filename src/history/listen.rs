6817112774a6a3de3d02cbabe481940ac136d58b
Derrick Plotsky <derrickplotsky@gmail.com>
2021-11-25 12:26:50 -0800

    Add helper build method from ListenAdded
    I couldn't use a From implementation because it needs the ID that it
    should be built with as well.

--- src/history/listen.rs
+++ src/history/listen.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 
+use crate::events::listen_added::ListenAdded;
+
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Listen {
     pub id: String,
@@ -7,4 +9,16 @@ pub struct Listen {
     pub track_name: String,
     pub ms_played: u64,
     pub end_time: String,
+}
+
+impl Listen {
+    pub fn build(id: &str, listen_added: &ListenAdded) -> Self {
+        Self {
+            id: id.to_string(),
+            artist_name: listen_added.artist_name.clone(),
+            track_name: listen_added.track_name.clone(),
+            ms_played: listen_added.ms_played,
+            end_time: listen_added.end_time.clone(),
+        }
+    }
 }
