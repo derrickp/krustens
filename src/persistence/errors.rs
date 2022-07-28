3895c91a3e8547b9ca63b51b8f53e150a40eb39d
Derrick Plotsky <derrickplotsky@gmail.com>
2022-07-27 19:10:24 -0700

    Delete unused files

--- src/persistence/errors.rs
+++
@@ -1,7 +0,0 @@
-use thiserror::Error;
-
-#[derive(Error, Debug)]
-pub enum PersistenceError {
-    #[error("persistence failed `{0}`")]
-    General(String),
-}
--- src/persistence/mod.rs
+++ src/persistence/mod.rs
@@ -1,13 +1,9 @@
-mod bootstrap;
-mod errors;
 mod file_writer;
 mod read_error;
 mod reader;
 mod write_error;
 mod writer;
 
-pub use bootstrap::bootstrap;
-pub use errors::PersistenceError;
 pub use file_writer::{FileType, FileWriter};
 pub use read_error::ReadError;
 pub use reader::Reader;
