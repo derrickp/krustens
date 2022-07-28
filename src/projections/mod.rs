4929d249dbf41ea18eca75405a711548686d5168
Derrick Plotsky <derrickplotsky@gmail.com>
2022-07-27 19:24:00 -0700

    Remove trait

--- src/projections/mod.rs
+++ src/projections/mod.rs
@@ -1,10 +1,8 @@
 mod has_listen;
 mod listen_tracker;
-mod projection_repository;
 mod sqlite_repository;
 pub mod stats;
 
 pub use has_listen::HasListen;
 pub use listen_tracker::{build_id, ListenTracker};
-pub use projection_repository::ProjectionRepository;
 pub use sqlite_repository::listen_tracker_repo;
--- src/projections/projection_repository.rs
+++
@@ -1,9 +0,0 @@
-use async_trait::async_trait;
-
-use crate::stores::EventStore;
-
-#[async_trait]
-pub trait ProjectionRepository<T: std::marker::Sync> {
-    async fn get(&mut self, store: &(impl EventStore + Send + std::marker::Sync)) -> T;
-    async fn flush(&mut self);
-}
