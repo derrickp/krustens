cef832aeff64f3571c6804bae4434696fa0c7b67
Derrick Plotsky <derrickplotsky@gmail.com>
2022-11-11 11:01:51 -0800

    Transform spotify and enum to Normalized
    We can now make a normalized version of all of these. Now they can be
    more easily used by the command handler.

--- src/track_plays/spotify.rs
+++ src/track_plays/spotify.rs
@@ -1,7 +1,11 @@
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use std::u64;
 
-#[derive(Serialize, Deserialize, Clone)]
+use crate::utils::parse_spotify_end_time;
+
+use super::Normalized;
+
+#[derive(Debug, Deserialize, Clone)]
 pub struct Spotify {
     #[serde(rename = "endTime")]
     pub end_time: String,
@@ -11,4 +15,24 @@ pub struct Spotify {
     pub track_name: String,
     #[serde(rename = "msPlayed")]
     pub ms_played: u64,
+}
+
+impl TryInto<Normalized> for Spotify {
+    type Error = ();
+
+    fn try_into(self) -> Result<Normalized, Self::Error> {
+        let end_time = match parse_spotify_end_time(&self.end_time) {
+            Ok(it) => it,
+            Err(_) => return Err(()),
+        };
+
+        Ok(Normalized {
+            end_time,
+            artist_name: self.artist_name,
+            track_name: self.track_name,
+            service_hint: "spotify".to_string(),
+            ms_played: Some(self.ms_played),
+            track_ms: None,
+        })
+    }
 }
--- src/track_plays/track_play.rs
+++ src/track_plays/track_play.rs
@@ -1,8 +1,20 @@
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 
-use super::Spotify;
+use super::{apple_music, Normalized, Spotify};
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Debug, Deserialize, Clone)]
 pub enum TrackPlay {
     Spotify(Spotify),
+    AppleMusicPlayActivity(apple_music::PlayActivity),
+}
+
+impl TryInto<Normalized> for TrackPlay {
+    type Error = ();
+
+    fn try_into(self) -> Result<Normalized, Self::Error> {
+        match self {
+            TrackPlay::Spotify(it) => it.try_into(),
+            TrackPlay::AppleMusicPlayActivity(it) => it.try_into(),
+        }
+    }
 }
