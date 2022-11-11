9ab52f99c831bc1d41a99c1a1b767728c90433ae
Derrick Plotsky <derrickplotsky@gmail.com>
2022-11-11 10:18:20 -0800

    Add Normalized track play
    I'm going to be adding a track play for apple music soon, and so
    instead of having my command handle each one separately, I've decided
    to have one that normalizes the data from each into a single type that
    contains the information I need for the command/event to make it easier
    to reason about. Upcoming will be the transforms to this normalized for
    my specific types.

--- src/track_plays/mod.rs
+++ src/track_plays/mod.rs
@@ -1,5 +1,7 @@
+mod normalized;
 mod spotify;
 mod track_play;
 
+pub use normalized::Normalized;
 pub use spotify::Spotify;
 pub use track_play::TrackPlay;
---
+++ src/track_plays/normalized.rs
@@ -0,0 +1,48 @@
+use chrono::NaiveDateTime;
+
+pub struct Normalized {
+    pub end_time: NaiveDateTime,
+    pub artist_name: String,
+    pub track_name: String,
+    pub service_hint: String,
+    pub ms_played: Option<u64>,
+    pub track_ms: Option<u64>,
+}
+
+impl Normalized {
+    pub fn formatted_end_time(&self) -> String {
+        format!("{}", self.end_time.format("%Y-%m-%d %H:%M:%S"))
+    }
+
+    pub fn play_time(&self) -> u64 {
+        self.ms_played.unwrap_or_default()
+    }
+
+    pub fn is_skipped_by_percent(&self) -> bool {
+        self.ms_played
+            .zip(self.track_ms)
+            .map(|(ms_played, track_ms)| (ms_played as f64 / track_ms as f64) < 0.1)
+            .unwrap_or(false)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use chrono::NaiveDateTime;
+
+    use super::Normalized;
+
+    #[test]
+    fn is_skipped_by_percent() {
+        let normalized = Normalized {
+            end_time: NaiveDateTime::default(),
+            artist_name: "artist".to_string(),
+            track_name: "track".to_string(),
+            service_hint: "apple_music".to_string(),
+            ms_played: Some(330994),
+            track_ms: Some(357000),
+        };
+
+        assert!(!normalized.is_skipped_by_percent());
+    }
+}
