cdafb5ba7f57b0c53cb32e1a2a406278082d81e7
Derrick Plotsky <derrickplotsky@gmail.com>
2022-11-19 13:42:38 -0800

    Move artist counts writing

---
+++ src/persistence/artist_counts.rs
@@ -0,0 +1,24 @@
+use crate::projections::statistics::{ArtistsCounts, FileName, Folder};
+
+use super::{FileWriter, Writer};
+
+pub async fn write_artists_counts(stats_folder: &Folder, stats: &ArtistsCounts, count: usize) {
+    stats_folder.create_if_necessary();
+
+    FileWriter::yaml_writer(stats_folder.file_name(&FileName::General))
+        .write(&stats.general_stats(count))
+        .await
+        .unwrap();
+    FileWriter::from(stats_folder.file_name(&FileName::Complete))
+        .write(&stats.all())
+        .await
+        .unwrap();
+    FileWriter::from(stats_folder.file_name(&FileName::Top50))
+        .write(&stats.top(50))
+        .await
+        .unwrap();
+    FileWriter::from(stats_folder.file_name(&FileName::Top100))
+        .write(&stats.top(100))
+        .await
+        .unwrap();
+}
--- src/persistence/mod.rs
+++ src/persistence/mod.rs
@@ -1,9 +1,11 @@
+mod artist_counts;
 mod event_store;
 mod file_writer;
 mod reader;
 pub mod sqlite;
 mod writer;
 
+pub use artist_counts::write_artists_counts;
 pub use event_store::EventStore;
 pub use file_writer::{FileType, FileWriter};
 pub use reader::Reader;
