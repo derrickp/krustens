8d8b5ca09f062a56bb00a75bce09a46fc0b6c717
Derrick Plotsky <derrickplotsky@gmail.com>
2022-11-29 22:04:57 -0800

    Format

--- src/interactive/mod.rs
+++ src/interactive/mod.rs
@@ -1,7 +1,7 @@
-mod app_state;
 mod app_message_set;
+mod app_state;
 mod full;
 
-pub use app_state::{AppMode, AppState};
 pub use app_message_set::AppMessageSet;
+pub use app_state::{AppMode, AppState};
 pub use full::full_ui;
