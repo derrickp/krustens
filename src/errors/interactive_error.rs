03828873c58ab20f046b4ff23f114c74af10dba8
Derrick Plotsky <derrickplotsky@gmail.com>
2023-01-09 19:29:24 -0800

    Remove unused error

--- src/errors/interactive_error.rs
+++ src/errors/interactive_error.rs
@@ -8,8 +8,6 @@ pub enum InteractiveError {
     Crossterm { message: String },
     #[error("tui Error {message:?}")]
     TuiError { message: String },
-    #[error("Required parameter ({name:?}) not set")]
-    RequiredParameterNotSet { name: String },
     #[error("parsing Error {message:?}")]
     ParsingIssue { message: String },
     #[error("error getting events {error:?}")]
